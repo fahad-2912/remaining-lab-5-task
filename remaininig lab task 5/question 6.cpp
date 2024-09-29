f7d20dfe55f39aae688950b37a1913c2e0ccf8de
fahad-2912 <khanfahadullah76@gmail.com>
2024-09-29 23:49:37 +0500

    Update question 6.cpp

--- remaininig lab task 5/question 6.cpp
+++ remaininig lab task 5/question 6.cpp
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int countOnes(int n) {
+    if (n == 0)
+        return 0;
+    return (n & 1) + countOnes(n >> 1);
+}
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    int result = countOnes(number);
+    printf("Number of 1s in binary representation of %d is: %d\n", number, result);
+    return 0;
+}
