cedadb6a8a79ca5462d4f934496d9797eecb4b68
fahad-2912 <khanfahadullah76@gmail.com>
2024-09-29 23:20:52 +0500

    Update question 6.cpp

--- remaininig lab task 5/question 6.cpp
+++ remaininig lab task 5/question 6.cpp
@@ -1,16 +1,1 @@
-#include <stdio.h>
-
-int main() {
-    int num, count = 0;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    while (num) {
-        count += num & 1;  
-        num >>= 1;        
-    }
-    printf("Number of 1s in binary: %d\n", count);
-    
-    return 0;
-}
-
 
