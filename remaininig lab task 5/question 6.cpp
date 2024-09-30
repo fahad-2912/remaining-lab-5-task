e3ca57667f9e855d6215d23d5b2f1ee12f10d74b
fahad-2912 <khanfahadullah76@gmail.com>
2024-09-30 22:13:33 +0500

    Update question 6.cpp

--- remaininig lab task 5/question 6.cpp
+++ remaininig lab task 5/question 6.cpp
@@ -1,10 +1,22 @@
 #include <stdio.h>
-int countOnes(int n) {
-    if (n == 0)
-        return 0;
-    return (n & 1) + countOnes(n >> 1);
+
+int countOnes(int a) {
+    int count = 0;
+    count += (a & 1);
+    count += (a >> 1) & 1;
+    count += (a >> 2) & 1;
+    count += (a >> 3) & 1;
+    count += (a >> 4) & 1;
+    count += (a >> 5) & 1;
+    count += (a >> 6) & 1;
+    count += (a >> 7) & 1;
+    count += (a >> 8) & 1;
+    count += (a >> 9) & 1;
+    count += (a >> 10) & 1;
+    count += (a >> 11) & 1;
+    return count;
 }
-int main() {
+    int main() {
     int number;
     printf("Enter a number: ");
     scanf("%d", &number);
